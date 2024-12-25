b7f484e58ad6cd26ddfee583c10db3090605ba32
fitronansori <fitronansori7@gmail.com>
2024-12-25 21:17:40 +0800

    Add Logo component

---
+++ src/components/common/Logo.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+const Logo = () => {
+  return (
+    <Link href={"/"}>
+      <h1 className="text-2xl text-primary font-unlock">JHONTRAKTOR</h1>
+    </Link>
+  );
+};
+export default Logo;
