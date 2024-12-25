618a89609370f7b2edc53d222a0dd50ab13a9211
fitronansori <fitronansori7@gmail.com>
2024-12-25 21:47:24 +0800

    Update navigation link from "Project" to "Projects"

--- src/components/layouts/Header/NavLinks.tsx
+++ src/components/layouts/Header/NavLinks.tsx
@@ -8,7 +8,7 @@ const navItems = [
   { id: 1, name: "Home", path: "/" },
   { id: 2, name: "About Us", path: "/about" },
   { id: 3, name: "Services", path: "/services" },
-  { id: 4, name: "Project", path: "/project" },
+  { id: 4, name: "Projects", path: "/projects" },
 ];
 
 type NavLinksProps = {
