b248a8920d6527c2163ade29a534a8783d0cce0c
fitronansori <fitronansori7@gmail.com>
2024-12-25 21:47:08 +0800

    Add HomeLayout component with Header integration

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/layouts/Header";
+
+const HomeLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <div>
+      <Header />
+      {children}
+    </div>
+  );
+};
+export default HomeLayout;
