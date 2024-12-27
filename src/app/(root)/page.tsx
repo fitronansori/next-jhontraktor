e47fb5781452fa328632a2e31a8cac5acd3ac1d3
fitronansori <fitronansori7@gmail.com>
2024-12-27 16:11:44 +0800

    Add Experience component to Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,11 @@
+import Experience from "@/components/layouts/Experience";
 import Hero from "@/components/layouts/Hero";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Experience />
     </>
   );
 };
